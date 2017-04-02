0ec8123363536e1c0f68862250a6cd59dbb300cb
thomasM <mouchelet.thomas@gmail.com>
2017-04-02 18:13:22 +0200

    intégration du bundle FosUser

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -11,28 +11,13 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class SecurityController extends Controller
+class SecurityController extends Bundle
 {
-    /**
-     * @Route("/login", name="login")
-     */
-    public function loginAction(Request $request)
+    public function getParent()
     {
-        // Si le visiteur est déjà identifié, on le redirige vers l'accueil
-        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return $this->redirectToRoute('admin');
-        }
-
-        // Le service authentication_utils permet de récupérer le nom d'utilisateur
-        // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
-        // (mauvais mot de passe par exemple)
-        $authenticationUtils = $this->get('security.authentication_utils');
-
-        return $this->render('blog/security/login.html.twig', array(
-            'last_username' => $authenticationUtils->getLastUsername(),
-            'error' => $authenticationUtils->getLastAuthenticationError(),
-        ));
+        return 'FOSUserBundle';
     }
 
 }
